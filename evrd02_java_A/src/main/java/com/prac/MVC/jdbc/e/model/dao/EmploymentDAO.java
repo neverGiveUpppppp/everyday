785fc58884e11955b9fec6b35ea68a44851641c9
neverGiveUpppppp <csy6219@naver.com>
2023-01-01 23:49:31 +0900

    java jdbc mvc prace : template + properties

--- evrd02_java_A/src/main/java/com/prac/MVC/jdbc/e/model/dao/EmploymentDAO.java
+++ evrd02_java_A/src/main/java/com/prac/MVC/jdbc/e/model/dao/EmploymentDAO.java
@@ -7,7 +7,59 @@ import java.util.ArrayList;
 
 public class EmploymentDAO {
 
+
     public ArrayList<Employee> empSelectAll(){
+
+        ArrayList<Employee> al = null;
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rset = null;
+
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            con = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe","SCOTT","qrwe");
+
+            String query = "SELECT * FROM EMP";
+
+            stmt = con.createStatement();
+            rset = stmt.executeQuery(query);
+
+            al = new ArrayList<Employee>();
+            while(rset.next()){
+
+                int empNo = rset.getInt("EMPNO");
+                String eName = rset.getString("ENAME");
+                String job = rset.getString("JOB");
+                int mgr = rset.getInt("MGR");
+                Date hireDate = rset.getDate("HIREDATE");
+                int sal = rset.getInt("SAL");
+                int comm = rset.getInt("COMM");
+                int depNo = rset.getInt("DEPTNO");
+
+                Employee e = new Employee(empNo,eName,job,mgr,hireDate,sal,comm,depNo);
+                al.add(e);
+            }
+
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally{
+            try{
+                rset.close();
+                stmt.close();
+                con.close();
+            }catch(SQLException e){
+                e.printStackTrace();
+            }
+        }
+        return al;
+    }
+
+
+
+
+    public ArrayList<Employee> empSelectAll2(){
 
         ArrayList<Employee> al = null;
         Connection con = null;
--- evrd02_java_A/src/main/java/com/prac/MVC/jdbcTemplate/a/common/TemplateJDBC.java
+++ evrd02_java_A/src/main/java/com/prac/MVC/jdbcTemplate/a/common/TemplateJDBC.java
@@ -1,6 +1,13 @@
 package com.prac.MVC.jdbcTemplate.a.common;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class TemplateJDBC {
 
@@ -8,13 +15,51 @@ public class TemplateJDBC {
 
     private TemplateJDBC(){}
 
-    public static Connection getConnection(){
 
+    // 내부 연결 방식
+    public static Connection getConnection1(){
 
+        if(con == null){
+            try{
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+                con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","MEMBER","qrwe");
 
+            }catch(ClassNotFoundException e){
+                e.printStackTrace();
+            }catch(SQLException e){
+                e.printStackTrace();
+            }
+        }
+        return con;
     }
 
 
+    // 외부파일 연결 방식
+    public static Connection getConnection2(){
+
+        if(con == null){
+            try {
+                Properties prop = new Properties();
+                prop.load(new FileReader("resources/dbDriver.properties"));
+
+                Class.forName(prop.getProperty("dbDriver"));
+                con = DriverManager.getConnection(prop.getProperty("url"),
+                                                  prop.getProperty("username"),
+                                                  prop.getProperty("password")
+                                                );
+            }catch(FileNotFoundException e){
+                e.printStackTrace();
+            }catch(ClassNotFoundException e){
+                e.printStackTrace();
+            }catch(SQLException e){
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return con;
+
+    }
 
 
 
