4c05e02c20f92d687463a20115290f0b9319b298
neverGiveUppppp <csy6219@naver.com>
2023-02-01 23:07:18 +0900

    Spring prac : bcrypt encode + insertMember(post)

--- evrd04_Spring/Spring01_kh/src/main/java/com/kh/b/member/controller/MemberController.java
+++ evrd04_Spring/Spring01_kh/src/main/java/com/kh/b/member/controller/MemberController.java
@@ -1,11 +1,14 @@
 package com.kh.b.member.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -87,7 +90,7 @@ public class MemberController {
 	
 	
 	@RequestMapping("minsert.me")
-	public void insertMember(@RequestParam(value="id", required=true) String id,
+	public void insertMember1(@RequestParam(value="id", required=true) String id,
 							 @RequestParam(value="name", required=true) String name,
 							 @RequestParam(value="pwd", required=true) String pwd,
 							 @RequestParam(value="pwd2", required=true) String pwd2,
@@ -95,7 +98,56 @@ public class MemberController {
 	}
 	
 	
-	
+//	@RequestMapping("minsert.me")
+	@RequestMapping(value="minsert.me", method=RequestMethod.POST)
+	public String insertMember2(@ModelAttribute MemberVO memberVo,
+								@RequestParam("post") String post,
+								@RequestParam("address1") String address1,
+								@RequestParam("address2") String address2) {
+		memberVo.setAddress(post + "/" + address1 + "/" + address2);
+		
+		
+		// 비번 암호화 작업
+//		System.out.println(memberVo.getPwd()); // 암호화 전
+		String encPwd = bcrypt.encode(memberVo.getPwd()); // encode() : 암호화가 된 비번을 반환하는 메소드
+		memberVo.setPwd(encPwd);
+//		System.out.println(memberVo.getPwd()); // 암호화 후
+		
+		
+		int result = mService.insertMember(memberVo);
+		if(result > 0) {
+			return "redirect:home.do";
+		}else {
+			throw new MemberException("회원 가입 실패");
+		}
+	}
+		
+// 비밀번호가 그대로 개발자에게 노출되기 때문에 문제됨
+// 스프링이 쓰는 보안이 뛰어난 방식 : BCrypt 암호화 방식
+// 		원본 데이터 --> salting, salt값 --> 암호화 데이터
+//						(random)
+//	원본 데이터에서 솔팅을 진행하면 얘는 암호화가 된 데이터가 발생함(다이제스트라고 부름)
+//	sha512도 마찬가지고 중간에 솔팅값이 들어가면서 원본 데이터가 암호화가 되는건데 솔팅값이 계쏙 동일하게 되면 a라는 데이터가 들어가면 게속 똑같은 암호화된 데이터가 들어가게 되면서 일정 패턴이 생기는 것
+//	비크립트는 솔팅값을 랜덤하게 생성
+//	그러다보니 똑같이 비밀번호를 집어넣어도 암호화된 데이터가 다 다르게 나옴
+//	jsp때는 항상 암호화가 같아서 db와 값을 비교만 했으면 됬으나 스프링에서는 다름
+
+	@RequestMapping(value="minsert.me",method=RequestMethod.POST)		
+	public String insertMember3(@ModelAttribute MemberVO memVo,
+								@RequestParam("post") String post,
+								@RequestParam("address1") String address1,
+								@RequestParam("address2") String address2) {
+		memVo.setAddress(post + "/" + address1 + "/" + address2);
+		String encPwd = bcrypt.encode(memVo.getPwd());
+		memVo.setPwd(encPwd);
+		
+		int result = mService.insertMember(memVo);
+		if(result > 0) {
+			return "redirect:home.do";
+		}else {
+			throw new MemberException("회원가입실패");
+		}
+	}
 	
 	
 	
