f54f51731ce76f03309a44c25f22c3bcceb27176
neverGiveUppppp <csy6219@naver.com>
2023-03-24 20:58:57 +0900

    상속 순서 알아보는 클래스 : 다시 돌려볼 것! 생성자 포함 돌아가는 순서파악 가능

---
+++ evrd02_java_B/src/main/java/com/prac/inheritance/Inheritance_moveTurn1.java
@@ -0,0 +1,25 @@
+package com.prac.inheritance;
+
+class A{
+    int a;
+
+    A(int a){ // A class의 생성자
+        this.a = a;
+    }
+    void display(){
+        System.out.println("a = "+a);
+    }
+}
+
+class B extends A{
+    B(int a){
+        super(a);
+        super.display();
+    }
+}
+
+public class Inheritance_moveTurn1 {
+    public static void main(String[] args) {
+        B obj = new B(10);
+    }
+}
---
+++ evrd02_java_B/src/main/java/com/prac/inheritance/Inheritance_moveTurn2.java
@@ -0,0 +1,43 @@
+package com.prac.inheritance;
+
+// 상속 관계에서 어떻게 코드가 움직이지는지 디버깅 찍어봐야하는 코드
+class SuperObject{
+    public void print(){
+        draw();
+    }
+    public void draw(){
+        draw();
+        System.out.println("프린트 : Super Object");
+    }
+}
+
+class SubObject extends SuperObject{
+    public void print(){
+        super.draw();
+    }
+    public void draw(){
+        System.out.println("프린트 : Sub Object");
+    }
+}
+
+
+public class Inheritance_moveTurn2{
+    public static void main(String[]args){
+
+        SuperObject superObj = new SubObject();
+        superObj.print();
+
+//        System.out.println(1%2); // 1
+//        System.out.println(2%4); // 2
+//        System.out.println(3%4); // 3
+//
+//        System.out.println(6%3); // 0
+//        System.out.println(3%6); // 3
+    }
+
+
+
+
+
+
+}
