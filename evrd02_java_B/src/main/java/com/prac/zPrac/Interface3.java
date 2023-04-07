8bb7855455e317cfd3e366f03a1091c67c4a1b22
neverGiveUppppp <csy6219@naver.com>
2023-04-07 13:49:54 +0900

    Interface practice : 추상화 개념 숙지 및 인터페이스에서 규칙을 정하고 하위 구현체에서 기능구현

---
+++ evrd02_java_B/src/main/java/com/prac/zPrac/Interface3.java
@@ -0,0 +1,71 @@
+package com.prac.zPrac;
+
+
+
+interface Brand{
+    String brand();
+}
+
+class Smartphone2{
+    String device;
+    void setDevice(String device){
+        this.device = device;
+    }
+    void checkPrint(){
+        System.out.println(this.device);
+    }
+    void app(){
+        System.out.println("application : " + this.device);
+    }
+}
+
+class Apple extends Smartphone2 implements Brand{
+    void app(){
+        System.out.println("apple 앱스토어 : " + this.device);
+    }
+
+    public String brand(){
+        return "apple";
+    }
+}
+
+class Samsung extends Smartphone2 implements Brand{
+    public String brand(){
+        return "samsung";
+    }
+}
+
+class Manufacturer{
+//    void produce(Apple apple){ 
+//        System.out.println("아이폰 생산");
+//    }
+//    void produce(Samsung samsung){
+//        System.out.println("갤럭시 생산");
+//    }
+    void produce(Brand brand){
+        System.out.println( brand.brand() + " 생산");
+    }
+}
+
+public class Interface3 {
+    public static void main(String[] args) {
+        Apple apple = new Apple();
+        apple.setDevice("iPhone XR");
+        apple.checkPrint();
+        apple.app();
+
+        System.out.println("==============================");
+
+        Samsung samsung = new Samsung();
+        samsung.setDevice("Galaxy");
+        samsung.checkPrint();
+        samsung.app();
+
+        System.out.println("==============================");
+
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.produce(apple);    // Apple class의 interface Brand가 추상화이기 때문에 apple을 받을 수 있는 것. implements했잖아
+        manufacturer.produce(samsung);
+
+    }
+}
