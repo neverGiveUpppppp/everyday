950fe9091e3a6e8079974b24403da58ecde17c18
neverGiveUppppp <csy6219@naver.com>
2023-04-10 18:05:25 +0900

    interface ex RemoteControl

--- evrd02_java_B/src/main/java/com/prac/Interface/Interface_EX_RemoteController.java
+++ evrd02_java_B/src/main/java/com/prac/Interface/Interface_EX_RemoteController.java
@@ -3,13 +3,101 @@ package com.prac.Interface;
 
 // 출처 : https://kjh95.tistory.com/137
 
+import java.rmi.Remote;
+
+/*
+1.인터페이스의 다형성
+2.객체를 직접 매개변수로 생성하여 넘기는 방법
+
+default 메서드
+정적(static) 메서드
+
+ */
 
 
+//  1.인터페이스의 다형성
+interface RemoteControl{
+
+    static final int MAX_VOLUMN = 10; // static final 생략가능. 컴파일 시, 자동추가해줌
+    static final int MIN_VOLUMN = 0;
+    public abstract void turnOn();    // abstract 생략가능. 컴파일 시, 자동추가해줌
+    public abstract void turnOff();
+    public abstract void setVolume(int volume);
+
+}
+
+
+class Audio implements RemoteControl{
+    private int volume;
+    @Override
+    public void turnOn(){
+        System.out.println("audio : turn on");
+    }
+    @Override
+    public void turnOff(){
+        System.out.println("audio : turn off");
+    }
+    @Override
+    public void setVolume(int volume){
+        if(volume > RemoteControl.MAX_VOLUMN){
+            System.out.println("최대볼륨은 10입니다");
+            this.volume = RemoteControl.MAX_VOLUMN;
+
+        }else if(volume < RemoteControl.MIN_VOLUMN){
+            System.out.println("최소볼륨은 0입니다");
+            this.volume = RemoteControl.MIN_VOLUMN;
+        }else{
+            this.volume = volume;
+        }
+        System.out.println("현재 볼륨 : "+volume);
+    }
+}
+class Television implements RemoteControl{
+    private int volume;
+    @Override
+    public void turnOn(){
+        System.out.println("TV : turn on");
+    }
+    @Override
+    public void turnOff(){
+        System.out.println("TV : turn off");
+    }
+    @Override
+    public void setVolume(int volume){
+        if(volume > RemoteControl.MAX_VOLUMN){
+            System.out.println("최대볼륨은 10입니다");
+            this.volume = RemoteControl.MAX_VOLUMN;
+        }else if(volume < RemoteControl.MIN_VOLUMN){
+            System.out.println("최소볼륨은 0입니다");
+            this.volume = RemoteControl.MIN_VOLUMN;
+        }else{
+            this.volume = volume;
+        }
+        System.out.println("현재 볼륨 : "+this.volume);
+    }
+    public void printMessage(){
+
+    }
+}
 
 
 
 public class Interface_EX_RemoteController {
     public static void main(String[] args) {
+        RemoteControl rControl = new Television();
+        rControl.turnOn();
+        rControl.setVolume(15);
+        rControl.setVolume(-100);
+        rControl.setVolume(8);
+        rControl.turnOff();
+        System.out.println();
 
+        rControl = new Audio(); // RemoteControl rControl에 선언한 변수명에 new객체를 새로해서 이어감
+        rControl.turnOn();
+        rControl.setVolume(10);
+        rControl.setVolume(-1);
+        rControl.turnOff();
+
+        // 나중에 이 구조를 MVC패턴으로 리팩토링해보자
     }
 }
