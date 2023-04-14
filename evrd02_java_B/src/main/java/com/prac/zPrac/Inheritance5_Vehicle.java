b58b0be6c247159014f6121f7932b924e63442ad
neverGiveUppppp <csy6219@naver.com>
2023-04-14 13:37:30 +0900

    abstract class inheritance prac : 부모클래스꺼를 오버라이드 안하면 자식껄로 안가고 부모껄로 찾아 올라감. 메소드 매개변수가 다르면 오버라이딩 안된다

---
+++ evrd02_java_B/src/main/java/com/prac/zPrac/Inheritance5_Vehicle.java
@@ -0,0 +1,42 @@
+package com.prac.zPrac;
+
+abstract class Vehicle{
+    String name;
+    abstract public String getName(String val);
+    public String getName(){
+        return "Vehicle name : " + name;
+    }
+}
+
+class Car extends Vehicle{
+
+    String name = null;
+
+    public Car(String car){
+        this.name = super.name = car;
+    }
+
+    @Override
+    public String getName(String val) {
+        return "Car name : " + val;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+}
+
+
+
+public class Inheritance5_Vehicle {
+    public static void main(String[] args) {
+        Vehicle obj = new Car("spark");
+        System.out.println(obj.getName());           // Vehicle name : spark
+        System.out.println(obj.getName("test")); // Car name : test
+        // 자식 클래스인 Car의 값이 아닌 부모 Vehicle의 클래스가 찍힌 이유?
+        // 자식 클래스에서 부모 클래스의 메소드를 오버라이딩하지 않으면, 부모 클래스의 메소드가 그대로 사용됨
+        // getName의 매개변수가 다르기 때문에 오버라이딩이 안된 것
+        //  getName()과 getName(String str)은 다른데, Car class에 getName(String str)만 있기 때문에
+        //  getName()을 찾아 부모클래스로 간 것. getName("test")를 찍으면 Car에서 잘 나오는 것을 볼 수 있다
+    }
+}
