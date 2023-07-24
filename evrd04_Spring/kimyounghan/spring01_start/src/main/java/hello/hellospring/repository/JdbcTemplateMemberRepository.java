c876c053b46d442f8f844a8fb351e4942b202dc0
neverGiveUppppp <csy6219@naver.com>
2023-07-24 18:13:08 +0900

    Feat : 홈화면 이동, 회원 등록조회(spring1 1time done)

---
+++ evrd04_Spring/kimyounghan/spring01_start/src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
@@ -0,0 +1,39 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcTemplateMemberRepository implements MemberRepository{
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateMemberRepository(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
--- evrd04_Spring/kimyounghan/spring01_start/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ evrd04_Spring/kimyounghan/spring01_start/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -1,29 +1,25 @@
 package hello.hellospring.service;
 
+
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class MemberServiceTest {
-    MemberService memberService;
-    MemberRepository memberRepository;
+@SpringBootTest
+@Transactional
+public class MemberServiceIntegrationTest {
 
-    @BeforeEach
-    public void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-    @AfterEach
-    public void afterEach() {
-//        memberRepository.clearStore();
-    }
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
 
     @Test
     public void 회원가입() throws Exception{
@@ -39,8 +35,9 @@ class MemberServiceTest {
         assertEquals(member.getName(), findMember.getName());
     }
 
+
     @Test
-    public void 중복_회원_예외() throws Exception{
+    public void 중복_회원_예외() throws Exception {
         // given
         Member member1 = new Member();
         member1.setName("spring");
@@ -50,10 +47,12 @@ class MemberServiceTest {
 
         // when
         memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class,() -> memberService.join(member2)); // 예외가 발생해야함
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
     }
+
+
 
 
 
