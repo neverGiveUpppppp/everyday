b8171b4e606883fe843f7b4a537c8a4d4d368c97
neverGiveUpppppp <csy6219@naver.com>
2023-10-02 01:22:57 +0900

    Refactor : spring2 section9-7 - 문제해결 with ObjectProvider 고객요청 받기 위한 ObjectProvider

--- evrd04_Spring/kimyounghan/spring02_core/src/main/java/hello/partial/web/LogDemoController.java
+++ evrd04_Spring/kimyounghan/spring02_core/src/main/java/hello/partial/web/LogDemoController.java
@@ -1,21 +1,49 @@
 package hello.partial.web;
 
 import hello.partial.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 // section9-6 request 스코프예제 만들기
+//@Controller
+//@RequiredArgsConstructor
+//public class LogDemoController {
+//    private final LogDemoService logDemoService;
+//    private final MyLogger myLogger;
+//
+//    @RequestMapping("log-demo") // log-demo라는 요청이 오면 아래의 logDemo()를 실행하라는 의미
+//    @ResponseBody   // 뷰화면 없이 문자를 바로 반환할거기에 @ResponseBody 사용
+//    public String logDemo(HttpServletRequest request) {
+//
+//    String requestURL = request.getRequestURL().toString();
+//        myLogger.setRequestURL(requestURL);
+//    }
+//}
+
+
+// section9-7 스코프와 Provider
 @Controller
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+//    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> objectProvider;
 
     @RequestMapping("log-demo") // log-demo라는 요청이 오면 아래의 logDemo()를 실행하라는 의미
     @ResponseBody   // 뷰화면 없이 문자를 바로 반환할거기에 @ResponseBody 사용
     public String logDemo(HttpServletRequest request) {
-
-    String requestURL = request.getRequestURL().toString();
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = objectProvider.getObject();
         myLogger.setRequestURL(requestURL);
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "ok";
     }
 }
+
+
+
--- evrd04_Spring/kimyounghan/spring02_core/src/main/java/hello/partial/web/LogDemoService.java
+++ evrd04_Spring/kimyounghan/spring02_core/src/main/java/hello/partial/web/LogDemoService.java
@@ -3,18 +3,30 @@ package hello.partial.web;
 
 import hello.partial.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 // section9-6 request 스코프예제 만들기
+//@Service
+//@RequiredArgsConstructor
+//public class LogDemoService {
+//    private final MyLogger myLogger;
+//
+//    public void logic(String id) {
+//        myLogger.log("service id = " + id);
+//    }
+//}
+
+
+// section9-7 스코프와 Provider
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final MyLogger myLogger;
+//    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> objectProvider;
 
     public void logic(String id) {
+        MyLogger myLogger = objectProvider.getObject();
         myLogger.log("service id = " + id);
     }
-
-    
-
 }
