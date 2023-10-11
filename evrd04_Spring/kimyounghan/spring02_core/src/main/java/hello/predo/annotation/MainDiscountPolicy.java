057eec634c38016a5f11865d5cc9f516daf57bc2
neverGiveUpppppp <csy6219@naver.com>
2023-10-12 02:15:16 +0900

    Feat : 사용자 정의 어노테이션 제작 및 @Quilifier 사용

---
+++ evrd04_Spring/kimyounghan/spring02_core/src/main/java/hello/predo/annotation/MainDiscountPolicy.java
@@ -0,0 +1,12 @@
+
+
+
+@Target({ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Docmented
+@Qualifier("mainDiscountPolicy")
+public @interface MaindiscountPolicy {
+
+
+}
