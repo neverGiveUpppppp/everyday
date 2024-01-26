9623cdb7bd68b90caea42463705330d2a9e905ac
nevergiveupppppp <csy6219@naver.com>
2024-01-26 20:45:10 +0900

    Practice : GA로 방문자수 붙이기 for GA4 API 시도

---
+++ evrd04_Spring/kimyounghan/GA/src/main/java/google_analytics/ga/visitor/AnalyticsDataFetcher.java
@@ -0,0 +1,45 @@
+package testing;
+
+import com.google.api.services.analyticsreporting.v4.AnalyticsReporting;
+import com.google.api.services.analyticsreporting.v4.model.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import com.google.api.services.analyticsreporting.v4.AnalyticsReporting;
+import com.google.api.services.analyticsreporting.v4.model.*;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class AnalyticsDataFetcher {
+    private AnalyticsReporting analyticsReporting;
+
+    public AnalyticsDataFetcher(AnalyticsReporting analyticsReporting) {
+        this.analyticsReporting = analyticsReporting;
+    }
+
+    public GetReportsResponse getVisitorData(String viewId) throws IOException {
+        DateRange dateRange = new DateRange();
+        dateRange.setStartDate("7daysAgo");
+        dateRange.setEndDate("today");
+
+        Metric sessionsMetric = new Metric()
+                .setExpression("ga:sessions")
+                .setAlias("sessions");
+
+        Dimension pageTitleDimension = new Dimension()
+                .setName("ga:pageTitle");
+
+        ReportRequest request = new ReportRequest()
+                .setViewId(viewId)
+                .setDateRanges(Arrays.asList(dateRange))
+                .setMetrics(Arrays.asList(sessionsMetric))
+                .setDimensions(Arrays.asList(pageTitleDimension));
+
+        GetReportsRequest getReport = new GetReportsRequest()
+                .setReportRequests(Arrays.asList(request));
+
+        return analyticsReporting.reports().batchGet(getReport).execute();
+    }
+}
+
