6d5675da1645203152b223c4775a9083b1901dfc
neverGiveUpppppp <csy6219@naver.com>
2024-03-31 22:30:28 +0900

    Practice : techtree batch

--- evrd04_Spring/example/SpringBatch/techtree/src/main/java/batch/techtree/BatchConfiguration.java
+++ evrd04_Spring/example/SpringBatch/techtree/src/main/java/batch/techtree/BatchConfiguration.java
@@ -1,12 +1,18 @@
 package batch.techtree;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -16,6 +22,11 @@ import javax.sql.DataSource;
 @Configuration
 @EnableBatchProcessing // Spring Batch Job 처리에 필요한 Bean들을 사용한다는 어노테이션 (여기서는 memory-based Database를 사용하고자 선언)
 public class BatchConfiguration {
+
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
 
     @Bean
     public FlatFileItemReader<Person> reader() {
@@ -56,6 +67,28 @@ ItemReader로 읽어온 데이터는 ItemProcessor를 통해 데이터를 변환
                 .dataSource(dataSource)
                 .build();
     }
+
+
+    @Bean
+    public Job importUserJob(JobCompletionNotificationListener listner, Step step1){
+        return jobBuilderFactory.get("importUserJob")
+                .incrementer(new RunIdIncrementer())
+                .listener(listner)
+                .flow(step1)
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step1(JdbcBatchItemWriter<Person> writer){
+        return stepBuilderFactory.get("step1")
+                .<Person, Person> chunk(10)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer)
+                .build();
+    }
+
 
 }
 
