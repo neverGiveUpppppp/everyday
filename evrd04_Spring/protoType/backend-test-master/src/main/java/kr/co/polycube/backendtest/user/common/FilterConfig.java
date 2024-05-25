1abe309eb11b2e60ce9d9e32e548b26888468bff
neverGiveUp <csy6219@naver.com>
2024-05-25 20:59:40 +0900

    Feat : FilterUrl 추가
    조건에서 요구하는 /users/{id}?name=test!!는 잡아냄
    테스트코드 작성하면서 더 테스트 해보기

---
+++ evrd04_Spring/protoType/backend-test-master/src/main/java/kr/co/polycube/backendtest/user/common/FilterConfig.java
@@ -0,0 +1,19 @@
+package kr.co.polycube.backendtest.user.common;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<FilterUrl> specialCharacterFilter() {
+        FilterRegistrationBean<FilterUrl> filter = new FilterRegistrationBean<>();
+        filter.setFilter(new FilterUrl());
+        filter.addUrlPatterns("/*");    // filter가 적용된 url 패턴
+        filter.setOrder(1);             // filter 간 작동 순서 지정(필터가 하나라면 생략가능)
+        return filter;
+    }
+
+}
---
+++ evrd04_Spring/protoType/backend-test-master/src/main/java/kr/co/polycube/backendtest/user/common/FilterUrl.java
@@ -0,0 +1,40 @@
+package kr.co.polycube.backendtest.user.common;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+public class FilterUrl implements Filter {
+
+    // 허용되지 않는 특수문자 패턴 정의
+    private static final Pattern DISALLOWED_PATTERN = Pattern.compile("[^a-zA-Z0-9?&=:.//]"); // 정규식 사용
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String query = httpRequest.getQueryString();
+
+        if (query != null && DISALLOWED_PATTERN.matcher(query).find()) {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "url에 ?, &, =, :, //를 제외한 특수문자는 사용이 불가능합니다.");
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
