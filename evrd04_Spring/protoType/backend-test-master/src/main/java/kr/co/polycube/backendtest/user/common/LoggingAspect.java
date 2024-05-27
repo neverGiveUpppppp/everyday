69194487a44e435d2687ce51869663f5622a8e7a
neverGiveUpppppp <csy6219@naver.com>
2024-05-27 23:52:48 +0900

    Feat : 로그용 AOP 추가
    header에서 User-Agent정보를 끌어와서 콘솔 로그에 표출

---
+++ evrd04_Spring/protoType/backend-test-master/src/main/java/kr/co/polycube/backendtest/user/common/LoggingAspect.java
@@ -0,0 +1,26 @@
+package kr.co.polycube.backendtest.user.common;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class LoggingAspect {
+
+    private final HttpServletRequest request;
+
+    @Before("execution(* kr.co.polycube.backendtest.user.UserController.*(..))")
+    public void logUserAgent() {
+        String userAgent = request.getHeader("User-Agent");
+        log.info("Client Agent: " + userAgent);
+    }
+
+}
