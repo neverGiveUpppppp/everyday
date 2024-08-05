6dc6d23a58f8f6331ca0300089c710993d115fd9
neverGiveUpppppp <csy6219@naver.com>
2024-08-06 01:37:09 +0900

    Practice : oracle Store Function 선언 및 사용

--- evrd01_sql/oracle_PLSQL.sql
+++ evrd01_sql/oracle_PLSQL.sql
@@ -52,10 +52,56 @@ BEGIN
     COMMIT;
 
 EXCEPTION WHEN OTHERSK THEN
-    ROLLBACK;K
+    ROLLBACK;
     DBMS_OUTPUT.PUT_LINE('응용 프로그램 오류 발생'||CHR(10)||SQLERRM);
 END;
 /
+
+
+
+/*********************************************************************/
+
+-- 저장함수
+CREATE OR REPLACE FUNCTION get_dept_employee_cnt(
+    a_dept_no NUMBER -- 사원 수를 계산할 부서번호
+)  RETURN NUMBER -- 부서의 사원수를 반환
+
+IS
+    -- 변수
+    v_cnt NUMBER; -- 건수
+
+BEGIN
+    --테이블 EMP에 들어있는 부서번호 a_deptno를 가진 사원수 계산
+    SELECT COUNT(*)
+        INTO v_cnt
+        FROM emp
+        WHERE dept_no = a_deptno;
+
+    RETURN v_cnt;
+
+EXCEPTION WHEN OTHERS THEN
+    DBMS_OUTPUT_LINE('응용 프로그램 오류' || CHR(10)||SQLERRM);
+    RETURN -1;
+END; -- 함수가 생성되었습니다.
+
+-- SELECT문을 사용한 저장 함수의 실행
+SELECT deptno 부서번호
+    , dname 부서명 
+    , loc 위치
+    , get_detp_employee_cnt(deptno) 사원수
+FROM dept;
+
+-- 익명 PL/SQL문을 사용한 저장 함수의 실행
+DECLARE
+    v_cnt PLS_INTEGER;
+BEGIN   
+    v_cnt := get_dept_employee_cnt(10);
+    DBMS_OUTPUT.PUT_LINE('사원 수 : '||v_cnt);
+END;
+/
+
+
+/*********************************************************************/
 
 
 
